b8779a19806840d8785404812db3c6dccfb11903
dckfan <109820204+dckfan@users.noreply.github.com>
2025-03-28 10:37:30 -0500

    maybe final change

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // output: "export", // Ensure static export is enabled
+};
+
+module.exports = nextConfig;
